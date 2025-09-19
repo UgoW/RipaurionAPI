45130730939b1ce604314c9f3233159aa56c4a4f
maximedervaux <maximeder.pro@gmail.com>
2025-09-19 10:34:57 +0200

    feat: :sparkles: Ajout de la gestion CORS et des modèles Pydantic

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
     return {"message": "Hello world"}
 
-# Call /check with url parameter
 @app.get("/check")
 def check_website(url: str):
     return {"url": url, "status": "checked"}
+
+@app.post("/check")
+def check_website_post(payload: dict):
+    print("Received payload:", payload)
+    url = payload.get("url")
+    return {"url": url, "urlstatus": "safe"}
